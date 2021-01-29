5b68e27a01ff8f920c6a7c771e5a1e5f65ba687a
Calin Tamas <calinntamas@gmail.com>
2021-01-29 16:17:03 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Toast from 'react-native-toast-message';
+
+function App() {
+  React.useEffect(() => {
+    Toast.show({
+      text1: 'Hello',
+      text2: 'Some other text',
+    });
+  }, []);
+
+  return (
+    <>
+      <Toast ref={(ref) => Toast.setRef(ref)} />
+    </>
+  );
+}
+
+export default App;
